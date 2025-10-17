772dec6287eb4759b61cddd2c0adfc852d8ba517
koiralasushant11-sketch <koiralasushant11@gmail.com>
2025-10-17 13:03:32 +0300

    Added another statement
    Added a line to call the printer function to output the users second name.

--- src/App.java
+++ src/App.java
@@ -4,9 +4,9 @@ public class App {
      public static void main(String[] args) throws Exception {
      Scanner in = new Scanner(System.in);
         
-        //asking printer to print first name
         System.out.println(" First number?");
         int first = in.nextInt();
+        //asking printer to print second name
         System.out.println("Last number?");
         int last = in.nextInt();
         
